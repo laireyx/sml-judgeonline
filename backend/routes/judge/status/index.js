4b2eb1e1067689b9b06762ea2759f188db0ce20e
laireyx <laireyx@gmail.com>
2022-03-20 20:26:38 +0900

    list 및 status 기능 구현

--- backend/routes/judge/status/index.js
+++ backend/routes/judge/status/index.js
@@ -1,5 +1,23 @@
+const fs = require("fs");
+const { promisify } = require("util");
 const { Router } = require("express");
+const { listSubmittedStatus } = require("../../../services/list");
 
 const route = Router();
+
+route.get("/:problemName", (req, res, next) => {
+  if (!req.params.problemName) {
+    res.status(400).send("No Problem Name");
+    return;
+  }
+
+  listSubmittedStatus(req.params.problemName).then(
+    (judgeStatus) => res.json(judgeStatus),
+    (err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  );
+});
 
 module.exports = route;
