2b1991a0cd8b8bb013f886146dd174aec2aeeb07
laireyx <laireyx@gmail.com>
2022-03-20 16:23:23 +0900

    Judge code 작성

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,15 @@ const path = require("path");
 const process = require("process");
 
 module.exports = {
+  /** Server configuration */
   PORT: process.env.PORT || 8100,
+
+  /** Directory configuration */
   JUDGE_HOME: process.env.JUDGE_HOME,
   UPLOAD_DIR: path.join(process.env.JUDGE_HOME, "upload"),
   SUBMIT_DIR: path.join(process.env.JUDGE_HOME, "submit"),
   VERIFY_DIR: path.join(process.env.JUDGE_HOME, "verify"),
+
+  /** Judge configurations */
+  JUDGE_TIMEOUT: 5000,
 };
---
+++ backend/services/judge/index.js
@@ -0,0 +1,7 @@
+const judgeSingleFile = require("./judge-single-file");
+const judgeSubmittedCode = require("./judge-submitted-code");
+
+module.exports = {
+  judgeSingleFile,
+  judgeSubmittedCode,
+};
