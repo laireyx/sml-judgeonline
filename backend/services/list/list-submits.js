da07809e19c10504201887ee5d69cb5e47290331
laireyx <laireyx@gmail.com>
2022-03-20 16:58:23 +0900

    Problem, Submit, Verification 목록을 얻어오는 코드 추가

---
+++ backend/services/list/list-submits.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+const config = require("../../config");
+const readdir = promisify(fs.readdir);
+
+/**
+ *
+ * @return {Promise<string[]>}
+ */
+module.exports = function listSubmits(problemName) {
+  return readdir(path.join(config.SUBMIT_DIR, problemName)).then((files) =>
+    files.filter((fileName) => fileName.endsWith(".sml"))
+  );
+};
