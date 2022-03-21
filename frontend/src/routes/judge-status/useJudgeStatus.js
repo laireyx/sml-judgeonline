9b560593283989fd586b28300f16df894610e76b
laireyx <laireyx@gmail.com>
2022-03-21 22:28:44 +0900

    Judge Status 대충 구현

--- frontend/src/routes/judge-status/useJudgeStatus.js
+++ frontend/src/routes/judge-status/useJudgeStatus.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function useJudgeStatus(problemName) {
-  const [judgeStatus, setJudgeStatus] = useState([]);
+  const [judgeStatus, setJudgeStatus] = useState({});
 
   useEffect(() => {
     fetch(`http://122.44.165.52:8100/api/judge/status/${problemName}`)
